541b52472518dcf8043a62b0120d8e17eac601c7
root <lorena@yeboyebo.es>
2017-07-12 08:53:18 +0000

    Cambio posicionamiento footer movil/web

---
+++ skin/frontend/tm_themes/theme348k/js/jquery-marte.js
@@ -0,0 +1,15 @@
+jQuery( document ).ready(function() {
+
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ){
+	    jQuery('#contenido-suscribe').appendTo('#col-1-footer');
+		jQuery('#help-me').appendTo('#col-2-footer');
+	}
+
+});
+
+
+
+
+
+
+
