63d982c5f5c12c5440fd2774f6324303ecd6e81f
root <lorena@yeboyebo.es>
2017-09-08 07:40:55 +0000

    cambios diseno gomage-checkout

--- skin/frontend/tm_themes/theme348k/js/jquery-marte.js
+++ skin/frontend/tm_themes/theme348k/js/jquery-marte.js
@@ -1,7 +1,14 @@
 jQuery( document ).ready(function() {
+	error_movil_web();
 	footer_movil_web();
 	jQuery(".youama-window-outside .close").click(function() {
-	jQuery("#header-account2").css("display","none");
+		jQuery("#header-account2").css("display","none");
+	});
+
+	jQuery("#open_coupon").click(function() {
+		jQuery( "#cupones" ).slideToggle( function() {
+    		// Animation complete.
+  		});
 	});
 });
 
@@ -15,5 +22,14 @@ function footer_movil_web() {
 
 function abrirlogin() {
 	jQuery("#header-account2").css("display","block");
+}
+
+function error_movil_web() {
+	var ventana_ancho = jQuery(window).width(); 	
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && ventana_ancho<='768'){
+	    jQuery('#imagen-1').appendTo('#col-1-error');
+		jQuery('#imagen-2').appendTo('#col-2-error');
+          jQuery('#texto').appendTo('#col-3-error');
+	} 
 }
 
