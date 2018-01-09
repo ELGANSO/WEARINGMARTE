61fb2f316b7303385ef8487a657316f21b759472
Ivan Vizcaino <ivan@yeboyebo.es>
2018-01-09 18:15:48 +0100

    Multiples precios para los productos

---
+++ app/code/local/Yeboyebo/MultiplePrices/Block/Prices.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan
+ * Date: 9/01/18
+ * Time: 12:33
+ */
+class Yeboyebo_MultiplePrices_Block_Prices extends Mage_Core_Block_Template
+{
+	public function getOptions()
+	{
+		return array('70','75','79');
+	}
+}
